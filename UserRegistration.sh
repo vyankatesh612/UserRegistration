6d42cb6dc918fa0a4ffc3dca106ac79eaea6e595
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-17 15:13:35 +0530

    ADDED : validation of first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
 echo "********USER REGISTRATION********"
+function firstname()
+	{ 
+		read -p  "Enter the first name : " firstname
+		pattern="^[A-Z][a-z]{3,}$"
+		if [[ $firstname =~ $pattern ]]
+		then
+			echo "valid"
+		else
+			echo "Invalid"
+		fi
+	}
+firstname
