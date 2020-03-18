789dcaa9f71c64155fc4016c25c226c99e871822
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-18 11:10:29 +0530

    MODIFIED : validation of first ,last name, mobile number and password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,54 +1,44 @@
 #!/bin/bash -x
 echo "********USER REGISTRATION********"
 
-function firstname()
-	{ 
-		read -p  "Enter the first name : " firstname
-		local pattern="^[A-Z][a-z]{3,}$"
-		if [[ $firstname =~ $pattern ]]
+function uservalidation()
+	{
+		local user=$1
+		local pattern=$2 
+		if [[ $user =~ $pattern ]]
 		then
 			echo "valid"
 		else
 			echo "Invalid"
 		fi
 	}
-firstname
 
-function lastname()
+function usermobile()
 	{
-		read -p "Enter the last name : " lastname
-		local pattern="^[A-Z][a-z]{3,}$"
-		if [[ $lastname =~ $pattern ]]
-		then
-			echo "valid"
-		else
-			echo "Invalid"
-		fi
-	}
-lastname
-
-function email()
-	{
-		read -p "Enter email address : " email
-		local pattern="^([a-zA-Z0-9]{1,}([.]?[a-zA-Z0-9])*[@][a-zA-Z]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]*)$"
-		if [[ $email =~ $pattern ]]
+		read -p "Enter mobile number : " mobilenumber
+		mobilepattern="[0-9]{2}[ ]?[0-9]{10}$"
+		if [[ $mobilenumber =~ $mobilepattern ]]
 		then
 			echo "valid"
 		else
 			echo "invalid"
 		fi
 	}
-email
 
-function mobilevalidation()
-	{
-		read -p "Enter the mobile number : " mobilenumber
-		local pattern="[0-9]{2}[ ]?[0-9]{10}$"
-		if [[ $mobilenumber =~ $pattern ]]
-		then
-			echo "valid"
-		else
-			echo "invalid"
-		fi
-	}
-mobilevalidation
+read -p "Enter the first name : " firstname
+firstnamepattern="^[A-Z][a-z]{3,}$"
+uservalidation $firstname $firstnamepattern
+
+read -p "Enter the last name : " lastname
+lastnamepattern="^[A-Z][a-z]{3,}$"
+uservalidation $lastname $lastnamepattern
+
+read -p "Enter the Email-id : " email
+emailpattern="^([a-zA-Z0-9]{1,}([.]?[a-zA-Z0-9])*[@][a-zA-Z]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]*)$"
+uservalidation $email $emailpattern
+
+usermobile
+
+read -p "Enter password : " password
+passwordpattern="[0-9a-zA-Z]{8,}$"
+uservalidation $password $passwordpattern
