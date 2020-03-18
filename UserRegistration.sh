18abc4ffaecd41237a4c2d6f9aedae4563984a2f
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-18 15:33:46 +0530

    MODIFIED :create seperate functions for each usecase

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+shopt -s extglob
+
 echo "********USER REGISTRATION********"
 
 function uservalidation()
@@ -21,24 +23,51 @@ function usermobile()
 		then
 			echo "valid"
 		else
-			echo "invalid"
+			echo "Invalid"
 		fi
 	}
 
-read -p "Enter the first name : " firstname
-firstnamepattern="^[A-Z][a-z]{3,}$"
-uservalidation $firstname $firstnamepattern
+function firstname()
+	{
+		read -p "Enter the first name : " firstname
+		firstnamepattern="^[A-Z][a-z]{3,}$"
+		uservalidation $firstname $firstnamepattern
+	}
+firstname
 
-read -p "Enter the last name : " lastname
-lastnamepattern="^[A-Z][a-z]{3,}$"
-uservalidation $lastname $lastnamepattern
+function lastname()
+	{
+		read -p "Enter the last name : " lastname
+		lastnamepattern="^[A-Z][a-z]{3,}$"
+		uservalidation $lastname $lastnamepattern
+	}
+lastname
 
-read -p "Enter the Email-id : " email
-emailpattern="^([a-zA-Z0-9]{1,}([.]?[a-zA-Z0-9])*[@][a-zA-Z]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]*)$"
-uservalidation $email $emailpattern
+function email()
+	{
+		read -p "Enter the Email-id : " email
+		emailpattern="^([a-zA-Z0-9]{1,}([.]?[a-zA-Z0-9])*[@][a-zA-Z]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]*)$"
+		uservalidation $email $emailpattern
+	}
+email
 
+function usermobile()
+	{
+		read -p "Enter mobile number : " mobilenumber
+		mobilepattern="[0-9]{2}[ ]?[0-9]{10}$"
+		if [[ $mobilenumber =~ $mobilepattern ]]
+		then
+			echo "valid"
+		else
+			echo "Invalid"
+		fi
+	}
 usermobile
 
-read -p "Enter password : " password
-passwordpattern="[0-9a-zA-Z]{8,}$"
-uservalidation $password $passwordpattern
+function password()
+	{
+		read -p "Enter password : " password
+		passwordpattern="^[?=.{8,}][?=.*[A-Z]].*$"
+		uservalidation $password $passwordpattern
+	}
+password
